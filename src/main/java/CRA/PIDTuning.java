6bbf1da84a71ed02b1414f1cdb826702617b4aaa
Tanmay <mail2tanmaygarg@gmail.com>
2025-07-01 13:10:30 -0500

    PID Tuning Class for realtime tuning done

--- src/main/java/CRA/PIDTuning.java
+++ src/main/java/CRA/PIDTuning.java
@@ -1,14 +1,21 @@
 package CRA;
 
+import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
 public class PIDTuning {
-    double kP;
-    double kI;
-    double kD;
-    double kF;
-    String mechanism;
+    private double kP;
+    private double kI;
+    private double kD;
+    private double kF;
+    private String mechanism;
+    private ShuffleboardTab tab;
+
+    private GenericEntry kPEntry;
+    private GenericEntry kIEntry; 
+    private GenericEntry kDEntry;
+    private GenericEntry kFEntry;
 
     public PIDTuning(String mechanism_name, double starting_kP, double starting_kI, double starting_kD, double starting_kF) {
         this.kP = starting_kP;
@@ -17,10 +24,22 @@ public class PIDTuning {
         this.kF = starting_kF;
         this.mechanism = mechanism_name;
 
-        ShuffleboardTab tab = Shuffleboard.getTab(mechanism);
+        tab = Shuffleboard.getTab(mechanism);
     }
 
     public void onDashboard() {
-        
+        kPEntry = tab.add(mechanism + " kP", kP).getEntry();
+        kIEntry = tab.add(mechanism + " kI", kI).getEntry();
+        kDEntry = tab.add(mechanism + " kD", kD).getEntry();
+        kFEntry = tab.add(mechanism + " kF", kF).getEntry();
+    }
+
+    public double[] getPID() {
+        return new double[] {
+            kPEntry.getDouble(kP), 
+            kIEntry.getDouble(kI), 
+            kDEntry.getDouble(kD), 
+            kFEntry.getDouble(kF)
+        };
     }
 }
