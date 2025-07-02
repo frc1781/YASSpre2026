52619fe4d2d67a23c2b9205b5c464b2200a0d884
Keith Van Rhein <kvanrhein@cps.edu>
2025-07-02 11:54:01 -0500

    options for vision
    Adding options for using vision and if using heading.

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -57,7 +57,7 @@ public class SwerveSubsystem extends SubsystemBase
 {
   private final SwerveDrive swerveDrive;
   private final boolean     visionDriveTest = true;
-  //private       Vision      vision;
+  private       Vision      vision;
 
   /**
    * Initialize {@link SwerveDrive} with the directory provided.
@@ -111,7 +111,9 @@ public class SwerveSubsystem extends SubsystemBase
 
   public void setupPhotonVision()
   {
-    //vision = new Vision(swerveDrive::getPose, swerveDrive.field);
+    if (Constants.USING_VISION) {
+      vision = new Vision(swerveDrive::getPose, swerveDrive.field);
+    }
   }
 
   @Override
@@ -121,7 +123,9 @@ public class SwerveSubsystem extends SubsystemBase
     if (visionDriveTest)
     {
       swerveDrive.updateOdometry();
-      //vision.updatePoseEstimation(swerveDrive);
+      if (Constants.USING_VISION) {
+        vision.updatePoseEstimation(swerveDrive);
+      }
     }
   }
 
