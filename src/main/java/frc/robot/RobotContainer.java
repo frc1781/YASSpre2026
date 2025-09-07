0b24a509d86a2edc5398f450c33fb63de3d1c9d1
Tanmay <mail2tanmaygarg@gmail.com>
2025-09-07 14:01:18 -0500

    commented out the tank drive line. I will put documentation on how to set up tank drive.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -34,7 +34,7 @@ public class RobotContainer
   final CommandXboxController driverXbox = new CommandXboxController(0);
   //private final Sensation sensation = new Sensation();
   private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve/robot"));
-  private final TankDriveTrain tankDrive = new TankDriveTrain(driverXbox);
+  // private final TankDriveTrain tankDrive = new TankDriveTrain(driverXbox);
   // private final Conveyor conveyor = new Conveyor();
   // private final Lights lights = new Lights();
   // private final Climber climber = new Climber();
